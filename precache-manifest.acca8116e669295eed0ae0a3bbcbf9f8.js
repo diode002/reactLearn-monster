1aa91d424b960f01ffc1c4dddb4e6b471f0c29fc
qaskhan02 <waqaskhanws2@gmail.com>
2019-11-30 11:50:31 +0500

    Updates

---
+++ precache-manifest.acca8116e669295eed0ae0a3bbcbf9f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38df2b26b241411e5dd2af315b7a47af",
+    "url": "/reactLearn-monster/index.html"
+  },
+  {
+    "revision": "332d3786301fe0fe9ff6",
+    "url": "/reactLearn-monster/static/css/main.6521f40c.chunk.css"
+  },
+  {
+    "revision": "d935b8e3403269409eb0",
+    "url": "/reactLearn-monster/static/js/2.722cd26d.chunk.js"
+  },
+  {
+    "revision": "332d3786301fe0fe9ff6",
+    "url": "/reactLearn-monster/static/js/main.36b718ad.chunk.js"
+  },
+  {
+    "revision": "8a2b9bb233f77b22b3fd",
+    "url": "/reactLearn-monster/static/js/runtime-main.7473acf8.js"
+  }
+]);
